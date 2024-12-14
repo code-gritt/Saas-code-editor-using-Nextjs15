87913836a4c6ebeccb86de9b3c92e4229d81f5b4
gokul <gokulchandan24@gmail.com>
2024-12-14 15:23:56 +0530

    setting up convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://integral-humpback-36.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import ConvexClientProvider from "@/components/providers/ConvexClientProvider";
 import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
 
@@ -32,7 +33,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-gradient-to-b from-gray-900 to-gray-950 text-gray-100 flex flex-col`}
         >
-          {children}
+          <ConvexClientProvider>{children}</ConvexClientProvider>
 
           <Footer />
 
